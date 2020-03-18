54f1cef93e6dd7027f9ea459d9350ba35ed505fa
Bharat Hinger <bharat3987@outlook.com>
2020-03-18 19:04:49 +0530

    Fix for object-invert exercise
    Add a condition to check for input object's own keys.

--- js-exercises/object-invert/objectInvert.js
+++ js-exercises/object-invert/objectInvert.js
@@ -3,7 +3,7 @@ function objectInvert(...args) {
   const inputObject = args[0];
   const invertedObject = {};
   for (const [key, value] of Object.entries(inputObject)) {
-    invertedObject[value] = key;
+    if (Object.prototype.hasOwnProperty.call(inputObject, key)) invertedObject[value] = key;
   }
   return invertedObject;
 }
